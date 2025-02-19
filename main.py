24abaa00c90ff0a72b3b2a0abc01fcbceb2efc2e
AgonySec <chenyuanhang2022@qq.com>
2025-02-19 15:48:04 +0800

    优化：代码结构

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 import argparse
 import logging
 import time
-from pwned_tool.google_search import search_google
-from pwned_tool.github_search import search_github
-from pwned_tool.email_leak import check_one_email,batch_process_emails_for
-from pwned_tool.pass_leak import check_pass_leak,batch_check_pass_leak
-from pwned_tool.utils import set_sensitiveWords,set_blacklistUsers
-from pwned_tool.hunter_search import search_hunter
+from LeakGuard.google_search import search_google
+from LeakGuard.github_search import search_github
+from LeakGuard.email_leak import check_one_email,batch_process_emails_for
+from LeakGuard.pass_leak import check_pass_leak,batch_check_pass_leak
+from LeakGuard.utils import set_sensitiveWords,set_blacklistUsers
+from LeakGuard.hunter_search import search_hunter
 
 
 # 设置日志
@@ -30,6 +30,42 @@ def parse_arguments():
     return parser.parse_args()
 
 
+def setup_blacklist_and_sensitive_words(args):
+    if args.bU:
+        BlacklistUsers = [x.strip('\n') for x in args.bU.readlines()]
+        set_blacklistUsers(BlacklistUsers)
+    if args.sW:
+        SensitiveWords = [x.strip('\n') for x in args.sW.readlines()]
+        set_sensitiveWords(SensitiveWords)
+
+def handle_github_search(args, output_file, mode):
+    if args.github_search:
+        search_github(query=args.github_search, output_file=output_file, mode=mode)
+    if args.github_file:
+        search_github(file=args.github_file, output_file=output_file, mode=mode)
+
+def handle_google_search(args, output_file, mode):
+    if args.google_search:
+        search_google(email_suffix=args.google_search, output_file=output_file, mode=mode)
+    if args.google_file:
+        search_google(file=args.google_file, output_file=output_file, mode=mode)
+
+def handle_email_leak(args, output_file, mode):
+    if args.email:
+        check_one_email(args.email, output_file, mode)
+    if args.email_file:
+        batch_process_emails_for(args.email_file, output_file, mode)
+
+def handle_password_leak(args):
+    if args.password:
+        check_pass_leak(args.password)
+    if args.passFile:
+        batch_check_pass_leak(args.passFile)
+
+def handle_hunter_search(args, output_file, mode):
+    if args.hunter_search:
+        search_hunter(domain=args.hunter_search, output_file=output_file, mode=mode)
+
 def main():
     start_time = time.time()
     args = parse_arguments()
@@ -40,48 +76,24 @@ def main():
     if args.email and args.email_file:
         logging.error("[!]请输入单个邮箱地址或文件路径，不要同时输入！")
         return
-    # 敏感字和黑名单
-    if args.bU:
-        BlacklistUsers = [x.strip('\n') for x in args.bU.readlines()]
-        set_blacklistUsers(BlacklistUsers)
-    if args.sW:
-        SensitiveWords = [x.strip('\n') for x in args.sW.readlines()]
-        set_sensitiveWords(SensitiveWords)
-    # 信息收集模式
+
+    setup_blacklist_and_sensitive_words(args)
+
     if args.c:
         print("todo")
 
-    # github关键字搜索
-    if args.github_search:
-        search_github(query=args.github_search, output_file=output_file,mode=mode)
-    if args.github_file:
-        search_github(file=args.github_file, output_file=output_file,mode=mode)
-    # 谷歌进行 邮箱后缀 搜索
-    if args.google_search:
-        search_google(email_suffix=args.google_search, output_file=output_file, mode=mode)
-    if args.google_file:
-        search_google(file=args.google_file, output_file=output_file,mode=mode)
-    # 邮箱泄露检测
-    if args.email:
-        check_one_email(args.email,output_file,mode)
-    if args.email_file:
-        batch_process_emails_for(args.email_file,output_file,mode)
-    # hunter检测邮箱
-    if args.hunter_search:
-        search_hunter(domain=args.hunter_search,output_file=output_file,mode=mode)
-    # 检测密码是否泄露
-    if args.password:
-        check_pass_leak(args.password)
-    if args.passFile:
-        batch_check_pass_leak(args.passFile)
+    handle_github_search(args, output_file, mode)
+    handle_google_search(args, output_file, mode)
+    handle_email_leak(args, output_file, mode)
+    handle_password_leak(args)
+    handle_hunter_search(args, output_file, mode)
 
-    if not any([args.email, args.email_file,args.passFile,args.hunter_search,args.password, args.google_search, args.google_file, args.c, args.github_search, args.github_file]):
+    if not any([args.email, args.email_file, args.passFile, args.hunter_search, args.password, args.google_search, args.google_file, args.c, args.github_search, args.github_file]):
         args.print_help()
 
     end_time = time.time()
     elapsed_time = end_time - start_time
     logging.info(f"[+]检测程序所花时间：{elapsed_time}")
-
 
 if __name__ == "__main__":
     main()
